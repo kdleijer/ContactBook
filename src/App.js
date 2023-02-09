3542abd93c2a7dddd4d6679e8c2e12b830c2c1f8
Pythadela <s.radomski19@gmail.com>
2023-02-09 23:16:36 +0100

    react-router-dom implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './bootstrap.min.css';
 
-function App() {
+import List from './components/List';
+import Add from './components/Add';
+import Update from './components/Update';
+
+import {Link, Routes, Route} from 'react-router-dom';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Link to="/">List</Link>|
+      <Link to="/add">Add</Link>|
+      <Link to="/update/1">Update</Link>
+      <Routes>
+        <Route path='/' element={<List/>} exact />
+        <Route path='/add' element={<Add/>} />
+        <Route path='/update/:id' element={<Update/>} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
