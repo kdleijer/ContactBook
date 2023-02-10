eeec5aa1811bd7343b78ced6ed61d3bc1736ba32
Pythadela <s.radomski19@gmail.com>
2023-02-10 21:13:01 +0100

    Create new contact functionality

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,63 +1,113 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-function Add(){
-    return (
-        <table className="table table-bordered">
-            <tr>
-                <th>ID</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>First name</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Last name</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Work phone</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Personal phone</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Address</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <th>Birthday</th>
-                <td>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td colSpan="2">
-                    <input type="submit" className="btn btn-dark" />
-                </td>
-            </tr>
-        </table>
-    );
-}
+class Add extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            id1:'',
+            first_name:'',
+            last_name:'',
+            email:'',
+            work_phone:'',
+            personal_phone:'',
+            address:'',
+            birthday:''
+        }
+        this.changeHandler=this.changeHandler.bind(this);
+        this.submitForm=this.submitForm.bind(this);
+    }
 
+    // Change Handler
+    changeHandler(event){
+        this.setState({
+            [event.target.name]:event.target.value
+        });
+    }
+
+    // Form
+    submitForm(){
+        fetch('http://127.0.0.1:8000/contact/',{
+            method:'POST',
+            body:JSON.stringify(this.state),
+            headers:{
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response=>response.json())
+        .then((data)=>console.log(data));
+
+        this.setState({
+            id1:'',
+            first_name:'',
+            last_name:'',
+            email:'',
+            work_phone:'',
+            personal_phone:'',
+            address:'',
+            birthday:''
+        });
+
+    }
+
+    render(){
+        return (
+            <table className="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <td>
+                            <input value={this.state.id1} name="id1" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>First name</th>
+                        <td>
+                            <input value={this.state.first_name} name="first_name" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Last name</th>
+                        <td>
+                            <input value={this.state.last_name} name="last_name" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td>
+                            <input value={this.state.email} name="email" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Work phone</th>
+                        <td>
+                            <input value={this.state.work_phone} name="work_phone" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Personal phone</th>
+                        <td>
+                            <input value={this.state.personal_phone} name="personal_phone" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Address</th>
+                        <td>
+                            <input value={this.state.address} name="address" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Birthday</th>
+                        <td>
+                            <input value={this.state.birthday} name="birthday" onChange={this.changeHandler} type="text" className="form-control" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2">
+                            <input type="submit" onClick={this.submitForm} className="btn btn-dark" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
 export default Add;
