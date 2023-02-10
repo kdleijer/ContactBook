842f85426374d7a6a6f522ecd3c43222f1ef0b1a
Pythadela <s.radomski19@gmail.com>
2023-02-10 20:23:32 +0100

    Fetch data with rest api

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function List(){
-    return (
-        <table className="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>First name</th>
-                    <th>Last name</th>
-                    <th>Email</th>
-                    <th>Work phone</th>
-                    <th>Personal phone</th>
-                    <th>Address</th>
-                    <th>Birthday</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>9999</td>
-                    <td>Example</td>
-                    <td>Example</td>
-                    <td>example@gmail.com</td>
-                    <td>999 999 999</td>
-                    <td>999 999 999</td>
-                    <td>Example</td>
-                    <td>05.02.2023</td>
-                    <td>
-                        <Link to="/update/1" className="btn btn-info mr-2">Update</Link>
-                        <button className="btn btn-danger">Delete</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    );
+class List extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            data:[]
+        };
+    }
+
+    fetchData(){
+        fetch('http://127.0.0.1:8000/contact/')
+        .then(response=>response.json())
+        .then((data)=>{
+            this.setState({
+                data:data
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    render(){
+        const contactData=this.state.data;
+        const rows=contactData.map((contact)=>
+            <tr key={contact.id}>
+                <td>{contact.id1}</td>
+                <td>{contact.first_name}</td>
+                <td>{contact.last_name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.work_phone}</td>
+                <td>{contact.personal_phone}</td>
+                <td>{contact.address}</td>
+                <td>{contact.birthday}</td>
+                <td>
+                    <Link to={"/update/"+contact.id} className="btn btn-info mr-2">Update</Link>
+                    <button className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        );
+        return (
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Email</th>
+                        <th>Work phone</th>
+                        <th>Personal phone</th>
+                        <th>Address</th>
+                        <th>Birthday</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    }
+
 }
 
 export default List;
