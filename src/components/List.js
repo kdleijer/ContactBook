94c3d18d195a9dca455c9e88fc83215ff1999234
Pythadela <s.radomski19@gmail.com>
2023-02-12 04:09:38 +0100

    Scroll table + static headers | UI resize + fixes

--- src/components/List.js
+++ src/components/List.js
@@ -108,7 +108,8 @@ class List extends React.Component{
             })
             .then(response => response.json())
             .then((data) => {
-                this.fetchData()
+                this.fetchData();
+                this.setState({ selectedForEdit: data.id, selectedForDeletion: null});
             });
         });
     }
@@ -118,42 +119,42 @@ class List extends React.Component{
             <tr key={contact.id}>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`contact_id-${contact.id}`} defaultValue={contact.contact_id} style={{width: 53, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`contact_id-${contact.id}`} defaultValue={contact.contact_id} style={{width: 42, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.contact_id}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`first_name-${contact.id}`} defaultValue={contact.first_name} style={{width: 118, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`first_name-${contact.id}`} defaultValue={contact.first_name} style={{width: 106, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.first_name}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`last_name-${contact.id}`} defaultValue={contact.last_name} style={{width: 148, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`last_name-${contact.id}`} defaultValue={contact.last_name} style={{width: 142, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.last_name}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`email-${contact.id}`} defaultValue={contact.email} style={{width: 278, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`email-${contact.id}`} defaultValue={contact.email} style={{width: 219, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.email}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`work_phone-${contact.id}`} defaultValue={contact.work_phone} style={{width: 133, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`work_phone-${contact.id}`} defaultValue={contact.work_phone} style={{width: 126, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.work_phone}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`personal_phone-${contact.id}`} defaultValue={contact.personal_phone} style={{width: 133, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`personal_phone-${contact.id}`} defaultValue={contact.personal_phone} style={{width: 127, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.personal_phone}
                 </td>
                 <td style={{textOverflow: 'ellipsis'}}>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`address-${contact.id}`} defaultValue={contact.address} style={{width: 283, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`address-${contact.id}`} defaultValue={contact.address} style={{width: 270, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.address}
                 </td>
                 <td>
                     {this.state.selectedForEdit === contact.id ?
-                        <input type="text" id={`birthday-${contact.id}`} defaultValue={contact.birthday} style={{width: 83, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
+                        <input type="text" id={`birthday-${contact.id}`} defaultValue={contact.birthday} style={{width: 86, marginTop: -5, marginLeft: -4, marginRight: -4}}/> :
                         contact.birthday}
                 </td>
                 <td style={{marginTop: -5, marginLeft: -4}}>
@@ -166,7 +167,7 @@ class List extends React.Component{
                           <button onClick={() => this.cancel(contact.id)} className="btn btn-secondary mr-2">Cancel</button>
                         </>)}
                     {this.state.selectedForDeletion === contact.id && ( <>
-                          <button onClick={() => this.deleteData(contact.id)} className="btn btn-danger mr-2">For sure?</button>
+                          <button onClick={() => this.deleteData(contact.id)} className="btn btn-danger mr-2">Sure?</button>
                           <button onClick={() => this.cancel(contact.id)} className="btn btn-secondary">Cancel</button>
                         </>)}
                 </td>
@@ -174,24 +175,27 @@ class List extends React.Component{
         );
 
         return (
-            <table className="table table-bordered" style={{minWidth: 1570, marginLeft: 15, marginRight: 15}}>
-                <thead>
+            <div style={{maxHeight: 500, overflow: "auto" }} className="contactGroup">
+                <h2 style={{  position: "absolute", top: 110, left: 65}}>No group</h2>
+            <table className="table table-bordered" style={{ width: "100%",minWidth: 1470, maxWidth: 1470, marginLeft: 60, marginRight: 60}}>
+                <thead >
                     <tr>
-                        <th style={{ width: 70 }}>ID</th>
-                        <th style={{ width: 135 }}>First name</th>
-                        <th style={{ width: 165 }}>Last name</th>
-                        <th style={{ width: 295 }}>Email</th>
-                        <th style={{ width: 150 }}>Work phone</th>
-                        <th style={{ width: 150 }}>Personal phone</th>
-                        <th style={{ width: 300 }}>Address</th>
+                        <th style={{ width: 58 }}>ID</th>
+                        <th style={{ width: 120 }}>First name</th>
+                        <th style={{ width: 155 }}>Last name</th>
+                        <th style={{ width: 230 }}>Email</th>
+                        <th style={{ width: 140 }}>Work phone</th>
+                        <th style={{ width: 140 }}>Personal phone</th>
+                        <th style={{ width: 280 }}>Address</th>
                         <th style={{ width: 100 }}>Birthday</th>
-                        <th style={{ width: 200 }}><button onClick={this.addContact} className="btn btn-success">Add contact</button></th>
+                        <th style={{ width: 200 }}><button onClick={this.addContact} className="btn btn-success">Add</button></th>
                     </tr>
                 </thead>
                 <tbody>
                     {rows}
                 </tbody>
             </table>
+            </div>
         );
     }
 }
