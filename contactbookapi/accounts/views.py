66d77184db7ac424dc2d4df53c2ecdade4ed303c
Pythadela <s.radomski19@gmail.com>
2023-03-01 20:29:30 +0100

    Refactoring...

--- contactbookapi/accounts/views.py
+++ contactbookapi/accounts/views.py
@@ -8,39 +8,48 @@ from .forms import CreateUserForm
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('home')
-    else:
+
+    if request.method != 'POST':
         form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-
-                return redirect('login')
-
         context = {'form': form}
         return render(request, 'register.html', context)
+
+    form = CreateUserForm(request.POST)
+    if form.is_valid():
+        form.save()
+        user = form.cleaned_data.get('username')
+        messages.success(request, 'Account was created for ' + user)
+        return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+
+def login_user(request, username, password):
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return True
+    return False
 
 
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
+
+    if request.method != 'POST':
+        return render(request, 'login.html', {})
+
+    form = request.POST
+    username = form.get('username')
+    password = form.get('password')
+
+    if login_user(request, username, password):
+        return redirect('home')
     else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+        messages.info(request, 'Username OR password is incorrect')
 
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
-
-        context = {}
-        return render(request, 'login.html', context)
+    return render(request, 'login.html', {})
 
 
 def logoutUser(request):
