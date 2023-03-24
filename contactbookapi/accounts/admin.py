a31b2af880fca4ff98ace357ceb9dd8ae998b13f
SzymCode <s.radomski19@gmail.com>
2023-03-24 20:49:23 +0100

    Users displayed on admin page

--- contactbookapi/accounts/admin.py
+++ contactbookapi/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login')
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
